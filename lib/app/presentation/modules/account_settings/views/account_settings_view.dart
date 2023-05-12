a42e7aa92fa0e360aedf678efd87d0e2ecdbe48a
FarisArmoush <fariskarmoush@gmail.com>
2023-05-12 15:39:06 +0300

    imitateing server waiting for delete account request button

--- lib/app/presentation/modules/account_settings/views/account_settings_view.dart
+++ lib/app/presentation/modules/account_settings/views/account_settings_view.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: use_if_null_to_convert_nulls_to_bools
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:groceries/app/config/localization/app_translations.dart';
-import 'package:groceries/app/config/routes/app_named_routes.dart';
 import 'package:groceries/app/data/repositories/firebase_auth_repository.dart';
 import 'package:groceries/app/presentation/modules/account_settings/widgets/logout_button.dart';
+import 'package:groceries/app/presentation/modules/account_settings/widgets/request_to_delete_account_button.dart';
 import 'package:groceries/app/presentation/modules/account_settings/widgets/user_data_list_tile.dart';
 
 class AccountSettingsView extends StatelessWidget {
@@ -58,12 +58,7 @@ class AccountSettingsView extends StatelessWidget {
           SizedBox(
             height: size.height * 0.05,
           ),
-          FilledButton(
-            onPressed: () => context.pushNamed(AppNamedRoutes.deleteAccount),
-            child: Text(
-              AppTranslations.deleteAccount,
-            ),
-          ),
+          const RequestToDeleteAccountButton(),
         ],
       ),
     );
