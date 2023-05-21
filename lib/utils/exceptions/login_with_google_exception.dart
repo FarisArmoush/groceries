5b6fc07e30848a065dffa93a4c8b9e88509132e3
FarisArmoush <fariskarmoush@gmail.com>
2023-05-21 13:49:26 +0300

    init folder and files for testing.

--- lib/utils/exceptions/login_with_google_exception.dart
+++ lib/utils/exceptions/login_with_google_exception.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 
-/// Creates a [LoginWithGoogleFailure] instance with a message based on the error code.
+/// Creates a [LoginWithGoogleException] instance with a message based on the error code.
 /// The following error codes are supported:
 ///
 /// * account-exists-with-different-credential: Account exists with different credentials.
@@ -18,49 +18,49 @@
 /// * invalid-verification-code: The credential verification code received is invalid.
 ///
 /// * invalid-verification-id: The credential verification ID received is invalid.
-class LoginWithGoogleFailure implements Exception {
-  /// Creates a new instance of [LoginWithGoogleFailure]
+class LoginWithGoogleException implements Exception {
+  /// Creates a new instance of [LoginWithGoogleException]
   /// with an optional error [message].
-  const LoginWithGoogleFailure([
+  const LoginWithGoogleException([
     this.message = 'An unknown exception occurred.',
   ]);
 
-  factory LoginWithGoogleFailure.fromCode(String code) {
+  factory LoginWithGoogleException.fromCode(String code) {
     switch (code) {
       case 'account-exists-with-different-credential':
-        return const LoginWithGoogleFailure(
+        return const LoginWithGoogleException(
           'Account exists with different credentials.',
         );
       case 'invalid-credential':
-        return const LoginWithGoogleFailure(
+        return const LoginWithGoogleException(
           'The credential received is malformed or has expired.',
         );
       case 'operation-not-allowed':
-        return const LoginWithGoogleFailure(
+        return const LoginWithGoogleException(
           'Operation is not allowed.  Please contact support.',
         );
       case 'user-disabled':
-        return const LoginWithGoogleFailure(
+        return const LoginWithGoogleException(
           'This user has been disabled. Please contact support for help.',
         );
       case 'user-not-found':
-        return const LoginWithGoogleFailure(
+        return const LoginWithGoogleException(
           'Email is not found, please create an account.',
         );
       case 'wrong-password':
-        return const LoginWithGoogleFailure(
+        return const LoginWithGoogleException(
           'Incorrect password, please try again.',
         );
       case 'invalid-verification-code':
-        return const LoginWithGoogleFailure(
+        return const LoginWithGoogleException(
           'The credential verification code received is invalid.',
         );
       case 'invalid-verification-id':
-        return const LoginWithGoogleFailure(
+        return const LoginWithGoogleException(
           'The credential verification ID received is invalid.',
         );
       default:
-        return const LoginWithGoogleFailure();
+        return const LoginWithGoogleException();
     }
   }
 
