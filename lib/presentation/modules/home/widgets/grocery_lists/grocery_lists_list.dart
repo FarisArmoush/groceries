362a34aa1e3490e79f7e000f0c61ce1349e010a7
FarisArmoush <fariskarmoush@gmail.com>
2023-07-07 10:37:09 +0300

    enhanced `GroceryListCard`

--- lib/presentation/modules/home/widgets/grocery_lists/grocery_lists_list.dart
+++ lib/presentation/modules/home/widgets/grocery_lists/grocery_lists_list.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:groceries/config/localization/app_translations.dart';
 import 'package:groceries/config/routes/app_named_routes.dart';
 import 'package:groceries/data/models/grocery_list_model.dart';
-import 'package:groceries/presentation/modules/home/widgets/grocery_lists/create_new_grocery_list_card_button.dart';
 import 'package:groceries/presentation/modules/home/widgets/grocery_lists/grocery_list_card.dart';
 import 'package:groceries/utils/extenstions/media_query_values.dart';
 
@@ -17,38 +15,28 @@ class GroceryListsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: context.deviceHeight * 0.31,
-      child: ListView.separated(
-        physics: const BouncingScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: lists.length + 1,
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(vertical: 4),
-        itemBuilder: (context, index) {
-          if (index == lists.length) {
-            return CreateNewGroceryListCardButton(
-              text: AppTranslations.createNewList,
-            );
-          } else {
-            return GroceryListCard(
-              listModel: GroceryListModel(
-                uid: lists[index].uid,
-                name: lists[index].name,
-                imageUrl: lists[index].imageUrl,
-                tasksAmount: lists[index].tasksAmount,
-                members: lists[index].members,
-              ),
-              onTap: () => context.pushNamed(AppNamedRoutes.groceryListDetails),
-            );
-          }
-        },
-        separatorBuilder: (context, index) {
-          return SizedBox(
-            width: context.deviceWidth * 0.01,
-          );
-        },
-      ),
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: lists.length,
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      itemBuilder: (context, index) {
+        return GroceryListCard(
+          listModel: GroceryListModel(
+            uid: lists[index].uid,
+            name: lists[index].name,
+            imageUrl: lists[index].imageUrl,
+            tasksAmount: lists[index].tasksAmount,
+            members: lists[index].members,
+          ),
+          onTap: () => context.pushNamed(AppNamedRoutes.groceryListDetails),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return SizedBox(
+          height: context.deviceHeight * 0.01,
+        );
+      },
     );
   }
 }
---
+++ lib/presentation/modules/home/widgets/grocery_lists/grocery_lists_rail_header.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:groceries/config/localization/app_translations.dart';
+import 'package:groceries/config/routes/app_named_routes.dart';
+import 'package:groceries/presentation/modules/home/widgets/rail_title.dart';
+import 'package:groceries/utils/constants/assets.gen.dart';
+
+class GroceryListsRailHeader extends StatelessWidget {
+  const GroceryListsRailHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        RailTitle(title: AppTranslations.myLists),
+        TextButton.icon(
+          label: Text(AppTranslations.createNewList),
+          onPressed: () => context.pushNamed(AppNamedRoutes.createList),
+          icon: Assets.svg.icPlusCircle.svg(
+            color: Theme.of(context).primaryColorLight,
+          ),
+        ),
+      ],
+    );
+  }
+}
