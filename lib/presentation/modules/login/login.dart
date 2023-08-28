5a2f4374a41e51cafab82def9ae989e2ffacd1ce
FarisArmoush <fariskarmoush@gmail.com>
2023-08-28 17:37:12 +0300

    deleted unnesecary imports. (i cant spell)

--- lib/presentation/modules/login/login.dart
+++ lib/presentation/modules/login/login.dart
@@ -1,7 +1,6 @@
 library login;
 
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
