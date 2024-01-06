d2fab0b176631bcae600ffe062d5b3e20783042a
Faris Armoush <75898233+FarisArmoush@users.noreply.github.com>
2024-01-06 19:45:34 +0300

    very_good groceries flutter app. (#206)

--- android/app/src/main/kotlin/com/example/groceries/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.groceries
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,17 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:groceries/app/app.dart';
 import 'package:groceries/config/services/remote_config_service.dart';
-import 'package:groceries/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<Widget> mainCommon() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   final remoteConfig = FirebaseRemoteConfig.instance;
   final remoteConfigService = RemoteConfigService(remoteConfig);
   await remoteConfigService.init();
