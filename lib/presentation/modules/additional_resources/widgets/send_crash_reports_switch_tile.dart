5fd78aff500c5cf4a5e6cfb05b60a698129991b6
Faris Armoush <fariskarmoush@gmail.com>
2024-01-29 19:28:40 +0300

    223-refactor-send-crash-reports-list-tile

--- lib/presentation/modules/additional_resources/widgets/send_crash_reports_switch_tile.dart
+++ lib/presentation/modules/additional_resources/widgets/send_crash_reports_switch_tile.dart
@@ -5,31 +5,17 @@ class SendCrashReportsSwitchTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SendCrashReportsCubit(),
-      child: const _SwitchListTile(),
-    );
-  }
-}
-
-class _SwitchListTile extends StatelessWidget {
-  const _SwitchListTile();
-
-  @override
-  Widget build(BuildContext context) {
     return BlocBuilder<SendCrashReportsCubit, bool>(
       buildWhen: (previous, current) => previous != current,
       builder: (context, state) {
-        return ListTile(
+        return SwitchListTile.adaptive(
+          value: state,
+          onChanged: (_) => context.read<SendCrashReportsCubit>().toggle(),
           tileColor: AppColors.transparent,
+          activeColor: context.theme.primaryColorLight,
           title: Text(AppTranslations.additionalResources.sendCrashReports),
           subtitle: Text(
             AppTranslations.additionalResources.sendCrashReportsDescription,
-          ),
-          trailing: CupertinoSwitch(
-            activeColor: context.theme.primaryColorLight,
-            value: state,
-            onChanged: (_) => context.read<SendCrashReportsCubit>().toggle(),
           ),
         );
       },
