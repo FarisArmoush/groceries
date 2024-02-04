d5ef32b76e341c1d3651c4f3da8d32d6cb3e358b
Faris Armoush <75898233+FarisArmoush@users.noreply.github.com>
2024-02-04 18:36:48 +0300

    260-fix-auth-views-paddings (#262)
    * initial impl.
    
    * 260-fix-auth-views-paddings

--- lib/presentation/modules/forgot_password/widgets/send_forgot_password_email_button.dart
+++ lib/presentation/modules/forgot_password/widgets/send_forgot_password_email_button.dart
@@ -24,7 +24,7 @@ class SendForgotPasswordEmailButton extends StatelessWidget {
         },
       ),
     ).symmetricPadding(
-      vertical: 12,
+      vertical: 24,
       horizontal: 24,
     );
   }
