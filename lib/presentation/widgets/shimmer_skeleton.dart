d8895968367597287f8d12e8185e6cb651efe827
Faris Armoush <fariskarmoush@gmail.com>
2024-02-28 21:01:21 +0300

    better shimmer colors for dark mode.

--- lib/presentation/widgets/shimmer_skeleton.dart
+++ lib/presentation/widgets/shimmer_skeleton.dart
@@ -19,50 +19,17 @@ class ShimmerSkeleton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Shimmer.fromColors(
       baseColor: context.isDarkMode
-          ? Colors.white.withOpacity(0.6)
+          ? AppColors.white.withOpacity(0.8)
           : Colors.grey.shade300,
       highlightColor: context.isDarkMode
-          ? Colors.white.withOpacity(0.4)
+          ? AppColors.white.withOpacity(0.6)
           : Colors.grey.shade100,
       child: Container(
         width: width,
         height: height,
         decoration: BoxDecoration(
           color: AppColors.white,
-          borderRadius: borderRadius ?? BorderRadius.circular(4),
-        ),
-      ),
-    );
-  }
-}
-
-class BackgroundShimmerSkeleton extends StatelessWidget {
-  const BackgroundShimmerSkeleton({
-    required this.height,
-    required this.width,
-    this.borderRadius,
-    super.key,
-  });
-
-  final double height;
-  final double width;
-  final BorderRadiusGeometry? borderRadius;
-
-  @override
-  Widget build(BuildContext context) {
-    return Shimmer.fromColors(
-      baseColor: context.isDarkMode
-          ? Colors.white.withOpacity(0.6)
-          : Colors.grey.shade300,
-      highlightColor: context.isDarkMode
-          ? Colors.white.withOpacity(0.4)
-          : Colors.grey.shade100,
-      child: Container(
-        width: width,
-        height: height,
-        decoration: BoxDecoration(
-          color: AppColors.white,
-          borderRadius: borderRadius ?? BorderRadius.circular(4),
+          borderRadius: borderRadius ?? BorderRadiusDirectional.circular(4),
         ),
       ),
     );
