367a8b23c528ba1bb18f6c5337ba1544ec0320b8
Faris Armoush <fariskarmoush@gmail.com>
2024-03-18 23:21:50 +0300

    better bottom sheet for grocery item details.

--- lib/presentation/modules/grocery_list_details/widgets/groceries_box_list.dart
+++ lib/presentation/modules/grocery_list_details/widgets/groceries_box_list.dart
@@ -4,6 +4,7 @@ import 'package:groceries/presentation/modules/grocery_list_details/widgets/groc
 import 'package:groceries/utils/constants/assets.gen.dart';
 import 'package:groceries/utils/extenstions/context_extensions.dart';
 import 'package:groceries_theme/app_theme.dart';
+import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
 
 class GroceriesBoxList extends StatelessWidget {
   const GroceriesBoxList({
@@ -22,44 +23,58 @@ class GroceriesBoxList extends StatelessWidget {
           color: context.theme.hintColor.withOpacity(0.75),
           context: context,
           tiles: list?.map(
-                (GroceryModel? item) => ListTile(
-                  tileColor: AppColors.transparent,
-                  title: Text(item?.name ?? ''),
-                  subtitle:
-                      item?.notes == null || (item?.notes?.isEmpty ?? false)
-                          ? null
-                          : Text(item?.notes ?? ''),
-                  trailing: IconButton(
-                    onPressed: () {},
-                    icon: item?.isDone ?? false
-                        ? Assets.icons.doubleCheck.svg(
-                            colorFilter: const ColorFilter.mode(
-                              Colors.green,
-                              BlendMode.srcIn,
+                (item) {
+                  final notesIsEmpty = item?.notes?.isEmpty ?? false;
+                  final notesIsNull = item?.notes == null;
+                  final isDone = item?.isDone ?? false;
+                  return ListTile(
+                    tileColor: AppColors.transparent,
+                    title: Text(item?.name ?? ''),
+                    subtitle: notesIsNull || notesIsEmpty
+                        ? null
+                        : Text(item?.notes ?? ''),
+                    trailing: IconButton(
+                      onPressed: () {},
+                      icon: isDone
+                          ? Assets.icons.doubleCheck.svg(
+                              colorFilter: const ColorFilter.mode(
+                                Colors.green,
+                                BlendMode.srcIn,
+                              ),
+                            )
+                          : Assets.icons.check.svg(
+                              colorFilter: ColorFilter.mode(
+                                context.theme.primaryColor,
+                                BlendMode.srcIn,
+                              ),
                             ),
-                          )
-                        : Assets.icons.check.svg(
-                            colorFilter: ColorFilter.mode(
-                              context.theme.primaryColor,
-                              BlendMode.srcIn,
-                            ),
-                          ),
-                  ),
-                  onTap: () => showModalBottomSheet<void>(
-                    context: context,
-                    elevation: 0,
-                    showDragHandle: true,
-                    useSafeArea: true,
-                    isScrollControlled: true,
-                    builder: (context) => GroceryItemDetailsBottomSheet(
-                      groceryModel: item,
                     ),
-                  ),
-                ),
+                    onTap: () => _showBottomSheet(context, item),
+                  );
+                },
               ) ??
               [],
         ).toList(),
       ),
+    );
+  }
+
+  void _showBottomSheet(BuildContext context, GroceryModel? item) {
+    if (item == null) return;
+    WoltModalSheet.show<void>(
+      context: context,
+      useSafeArea: false,
+      useRootNavigator: true,
+      pageListBuilder: (context) => [
+        SliverWoltModalSheetPage(
+          backgroundColor: context.theme.scaffoldBackgroundColor,
+          mainContentSlivers: [
+            GroceryItemDetailsBottomSheet(
+              groceryModel: item,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
