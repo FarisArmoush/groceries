f5556e4e81254a2eb3b611c3f5e14dcf6a6a67d4
Faris Armoush <fariskarmoush@gmail.com>
2024-04-28 15:50:29 +0300

    bugfix/ show additional resources button.

--- lib/presentation/modules/settings/views/settings_view.dart
+++ lib/presentation/modules/settings/views/settings_view.dart
@@ -11,26 +11,11 @@ import 'package:groceries/presentation/widgets/sliver_sized_box.dart';
 import 'package:groceries/utils/constants/assets.gen.dart';
 import 'package:groceries/utils/extenstions/context_extensions.dart';
 
-class SettingsView extends StatefulWidget {
+class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
   @override
-  State<SettingsView> createState() => _SettingsViewState();
-}
-
-class _SettingsViewState extends State<SettingsView> {
-  @override
-  void initState() {
-    context.read<RemoteConfigBloc>().add(
-          const RemoteConfigEvent.getShowAdditionalResources(),
-        );
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final showAdditionalResources =
-        context.read<RemoteConfigBloc>().state.showAdditionalResources;
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -64,25 +49,48 @@ class _SettingsViewState extends State<SettingsView> {
               SizedBox(
                 height: context.deviceHeight * 0.01,
               ),
-              if (showAdditionalResources) ...[
-                Visibility(
-                  visible: showAdditionalResources,
-                  child: SettingsListTile(
-                    title: Translations.additionalResources.title,
-                    subtitle: Translations.additionalResources.description,
-                    iconPath: Assets.icons.menu.path,
-                    onTap: () =>
-                        context.pushNamed(AppRoute.additionalResources.named),
-                  ),
-                ),
-                SizedBox(
-                  height: context.deviceHeight * 0.01,
-                ),
-              ],
+              const AdditionalResourcesButton(),
             ],
           ),
         ],
       ),
+    );
+  }
+}
+
+class AdditionalResourcesButton extends StatefulWidget {
+  const AdditionalResourcesButton({super.key});
+
+  @override
+  State<AdditionalResourcesButton> createState() =>
+      _AdditionalResourcesButtonState();
+}
+
+class _AdditionalResourcesButtonState extends State<AdditionalResourcesButton> {
+  @override
+  void initState() {
+    context.read<RemoteConfigBloc>().add(
+          const RemoteConfigEvent.getShowAdditionalResources(),
+        );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<RemoteConfigBloc, RemoteConfigState>(
+      buildWhen: (previous, current) =>
+          previous.showAdditionalResources != current.showAdditionalResources,
+      builder: (context, state) {
+        return Visibility(
+          visible: state.showAdditionalResources,
+          child: SettingsListTile(
+            title: Translations.additionalResources.title,
+            subtitle: Translations.additionalResources.description,
+            iconPath: Assets.icons.menu.path,
+            onTap: () => context.pushNamed(AppRoute.additionalResources.named),
+          ),
+        );
+      },
     );
   }
 }
