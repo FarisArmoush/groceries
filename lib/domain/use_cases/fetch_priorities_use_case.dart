0a585b2c472a3b19196027ae15e9593b21e28cd9
Faris Armoush <fariskarmoush@gmail.com>
2024-05-05 16:14:17 +0300

    342-feature-divide-constants-repositories

--- lib/domain/use_cases/fetch_priorities_use_case.dart
+++ lib/domain/use_cases/fetch_priorities_use_case.dart
@@ -1,15 +1,15 @@
 import 'package:groceries/data/models/priority_model/priority_model.dart';
-import 'package:groceries/domain/repositories/constants_repository.dart';
+import 'package:groceries/domain/repositories/priorities_repository.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
 class FetchPrioritiesUseCase {
-  const FetchPrioritiesUseCase(this._constantsRepository);
+  const FetchPrioritiesUseCase(this._prioritiesRepository);
 
-  final ConstantsRepository _constantsRepository;
+  final PrioritiesRepository _prioritiesRepository;
 
   Future<List<PriorityModel>> call() async {
-    final priorities = await _constantsRepository.fetchPriorities();
+    final priorities = await _prioritiesRepository.fetchPriorities();
     final sortedPriorities = priorities
       ..sort((a, b) => a.index?.compareTo(b.index ?? 0) ?? 1);
     return sortedPriorities;
