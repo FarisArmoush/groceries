2104c06eaa2addc2aec4354e7795a2ba8852644f
Faris Armoush <75898233+FarisArmoush@users.noreply.github.com>
2024-05-09 15:41:27 +0300

    332 chore make repository public (#348)
    Co-authored-by: hkarmoush <hkarmoush@gmail.com>

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,5 +1,4 @@
 import 'package:groceries/bootstrap.dart';
-import 'package:groceries/config/services/crashlytics_service.dart';
 import 'package:groceries/data/models/app_flavor/app_flavor.dart';
 import 'package:groceries/main_common.dart';
 
@@ -9,9 +8,6 @@ void main() {
       apiUrl: 'Production URL',
       environment: FlavorEnvironments.production,
     );
-    CrashlyticsService()
-      ..initCrashlytics()
-      ..initPlatformErrorsHandler();
     return mainCommon(flavor);
   });
 }
